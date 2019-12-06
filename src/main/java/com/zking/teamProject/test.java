01aa0f4ba18706131f4dfde190604c0af3c645c5
zhenghuiyi <912180117@qq.com>
2019-12-06 18:43:35 +0800

    项目空壳

--- src/main/java/com/zking/teamProject/test.java
+++ src/main/java/com/zking/teamProject/test.java
@@ -2,6 +2,6 @@ package com.zking.teamProject;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test!!!");
     }
 }
