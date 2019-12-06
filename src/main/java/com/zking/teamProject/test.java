674b0413027281bd955654b2a1a98f38ff189450
zhenghuiyi <912180117@qq.com>
2019-12-06 19:12:36 +0800

    测试

--- src/main/java/com/zking/teamProject/test.java
+++ src/main/java/com/zking/teamProject/test.java
@@ -3,6 +3,6 @@ package com.zking.teamProject;
 public class test {
     public static void main(String[] args) {
         System.out.println("test!!!");
-        System.out.println("杜鑫睿你好呀！");
+        System.out.println("杜鑫睿是傻逼！");
     }
 }
