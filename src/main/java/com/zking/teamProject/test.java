38a280100dd2d55dc9d76737dc84434c8fd8b2ae
duixinrui123456 <1115436882@qq.com>
2019-12-06 19:51:53 +0800

    test修改

--- src/main/java/com/zking/teamProject/test.java
+++ src/main/java/com/zking/teamProject/test.java
@@ -3,6 +3,6 @@ package com.zking.teamProject;
 public class test {
     public static void main(String[] args) {
         System.out.println("test!!!");
-        System.out.println("杜鑫睿是傻逼！！！");
+        System.out.println("小姨妹是傻逼！！！");
     }
 }
